2575a446a8515830015cb55d1aba35766030c73a
FL0R1AN <fl0r1an@waechter.koeln>
2026-01-26 20:40:24 +0100

    test: check website title

---
+++ src/components/__tests__/WebsiteTitle.spec.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import WebsiteTitle from '../WebsiteTitle.vue'
+
+describe('WebsiteTitle', () => {
+  it('renders properly', () => {
+    const wrapper = mount(WebsiteTitle)
+    expect(wrapper.text()).toContain('lebenswerter-guertel')
+  })
+})
