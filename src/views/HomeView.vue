82dba83e8e9c0319f130d58f342db9a4ae3126df
FL0R1AN <fl0r1an@waechter.koeln>
2026-02-01 00:38:07 +0100

    feat: add text and image

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,19 +5,30 @@ import WebsiteTitle from '../components/WebsiteTitle.vue'
 
 <template>
   <header class="bg-center">
-    <IconAnimation />
-    <WebsiteTitle />
+    <IconAnimation class="pl-4 md:pl-0" />
+    <WebsiteTitle class="pl-4 md:pl-0" />
   </header>
-  <main class="main"></main>
+  <main class="main">
+    <div>
+      <h2 class="text-4xl font-bold text-white py-8 pl-2 lg:pl-0">
+        Die Luxemburger ist wieder lebenswert.
+      </h2>
+      <img
+        alt="Bild mit der Aufschrift: Ab sofort Tempo 30 auf der Lux. Weniger Lärm, bessere Luft, mehr Sicherheit für alle Wir bedanken uns für die große Unterstützung aus den Veedeln"
+        src="/IGLux_Kinowerbung.jpg"
+      />
+      <h3 class="text-4xl font-bold text-white py-8 pl-2 lg:pl-0">
+        Jetzt machen wir den Gürtel wieder lebenswert!
+      </h3>
+    </div>
+  </main>
 </template>
 
 <style scoped>
 header {
   display: flex;
-  flex-direction: column;
   align-items: center;
   justify-content: center;
-  min-height: auto;
   padding: 2rem 0;
 }
 
@@ -26,8 +37,6 @@ header {
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  min-height: 100vh;
-  min-width: 100vw;
   background: linear-gradient(135deg, #009ee3 0%, #74b828 100%);
 }
 </style>
