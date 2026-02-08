5768fae1b5ef2103921f3ffc211908fed8b77063
FL0R1AN <fl0r1an@waechter.koeln>
2026-02-08 18:14:06 +0100

    feat: copy indicate icon for email

--- src/views/PrivacyPolicyView.vue
+++ src/views/PrivacyPolicyView.vue
@@ -9,7 +9,7 @@ const decryptEmail = () => {
 const copyEmailToClipboard = () => {
   const email = decryptEmail()
   navigator.clipboard.writeText(email).then(() => {
-    alert('Email copied to clipboard!')
+    alert('Die E-Mail-Adresse wurde in die Zwischenablage kopiert')
   })
 }
 </script>
@@ -42,9 +42,23 @@ const copyEmailToClipboard = () => {
           50968 Köln
         </p>
         <p>
-          E-Mail-Adresse:
+          E-Mail:
           <a class="email-obfuscated" rel="nofollow" @click="copyEmailToClipboard">
             {{ decryptEmail() }}
+            <svg
+              class="size-6"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1.5"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
           </a>
         </p>
       </section>
@@ -277,6 +291,15 @@ const copyEmailToClipboard = () => {
 </template>
 
 <style scoped>
+.email-obfuscated {
+  font-family: monospace;
+  padding: 0.25rem 0.5rem;
+  border-radius: 3px;
+  display: inline-flex;
+  align-items: center;
+  gap: 0.5rem;
+}
+
 .privacy-container {
   min-height: calc(100vh - 200px);
   padding: 1rem;
