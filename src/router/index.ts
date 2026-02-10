b9bd370dde6ca067c0f028d5b6ae8a8cd5e5cb9b
FL0R1AN <fl0r1an@waechter.koeln>
2026-02-10 23:32:52 +0100

    refactor: switch to hash history for router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,10 @@
-// src/router/index.ts
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ContactView from '../views/ContactView.vue'
 import PrivacyPolicyView from '@/views/PrivacyPolicyView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
