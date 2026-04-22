af5c0daa98a1bd5da186b2e6138dad1d6dc07dc9
FL0R1AN <fl0r1an@waechter.koeln>
2026-04-22 19:25:51 +0200

    feat: add missing import for @unhead/vue/client

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { createHead } from '@unhead/vue/client'
 
 const app = createApp(App)
 const head = createHead()
